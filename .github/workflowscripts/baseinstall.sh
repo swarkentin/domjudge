57e0ba894469cf8d57dab36c0a52c1684c27ef63
Sheldon Warkentin <sheldon.warkentin@gmail.com>
2022-02-07 12:43:43 -0700

    more debugging

--- .github/workflowscripts/baseinstall.sh
+++ .github/workflowscripts/baseinstall.sh
@@ -44,4 +44,8 @@ openssl req -nodes -new -x509 -keyout /tmp/server.key -out /tmp/server.crt -subj
 cat $(pwd)/.github/workflowscripts/nginx_extra | sudo tee -a /etc/nginx/sites-enabled/domjudge
 
 sudo systemctl restart nginx
+curl http://localhost/api/
+curl http://localhost/api
+curl http://localhost/domjudge/api/
+curl http://localhost/domjudge/api
 curl http://localhost/api/doc.json
