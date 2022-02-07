847c0eafdfce4202b312142710feec8e5ee058e0
Sheldon Warkentin <sheldon.warkentin@gmail.com>
2022-02-07 13:57:43 -0700

    Print logs after getting api

--- .github/workflowscripts/getapi.sh
+++ .github/workflowscripts/getapi.sh
@@ -4,4 +4,8 @@ set -eux
 
 curl --cacert /tmp/server.crt https://localhost/api/doc.json > ./openapi.json
 cat openapi.json
+
+# print the logs!
+cat /home/runner/domjudge/domserver/webapp/var/log/*
+
 python3 -m json.tool < ./openapi.json
